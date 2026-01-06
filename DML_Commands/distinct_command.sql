753b11af64d0610f851e1715ab46306efd2144a3
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-06 20:42:23 +0500

    querry modified

---
+++ DML_Commands/distinct_command.sql
@@ -0,0 +1,8 @@
+-- DISTINCT (unique) ---> To find unique 
+SELECT DISTINCT(brand_name) AS 'All brands' FROM mysqllab.smartphones;
+SELECT DISTINCT(model) AS 'All models'FROM mysqllab.smartphones;
+SELECT DISTINCT(processor_brand) AS 'ALll processors' FROM mysqllab.smartphones;
+
+-- DISTINCT combo
+SELECT DISTINCT brand_name, processor_brand FROM mysqllab.smartphones;
+SELECT DISTINCT brand_name AS 'All brands', model AS 'All models', processor_brand AS 'ALll processors' FROM mysqllab.smartphones;
