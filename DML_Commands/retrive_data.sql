fa781d1e4c4643fc9476def4f802b0eed2e7554a
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-06 19:10:17 +0500

    querry updated

--- DML_Commands/retrive_data.sql
+++ DML_Commands/retrive_data.sql
@@ -1,12 +1,21 @@
--- To select all data from the table. 
+-- To select all data from the table 
 SELECT * FROM mysqllab.smartphones; 
 
--- To get selective columns
+-- To get selective columns ---> Filter column
 SELECT brand_name, model, price, rating FROM mysqllab.smartphones;
-
 SELECT model, battery_capacity, os FROM mysqllab.smartphones;
 
 -- To alias (rename) a column
 SELECT os AS 'Operating_system' FROM mysqllab.smartphones;
 SELECT battery_capacity AS 'mAh' FROM mysqllab.smartphones;
 SELECT os AS 'Operating_system', battery_capacity AS 'mAh' FROM mysqllab.smartphones;
+
+-- To create expression
+SELECT model, 
+SQRT(resolution_width*resolution_width + resolution_height*resolution_height)/screen_size 
+AS PPI
+FROM mysqllab.smartphones;
+ 
+SELECT model, rating/10 As rate FROM mysqllab.smartphones;
+
+
