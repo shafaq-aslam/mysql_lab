c583c86562f7bb10f335f1d22a9b9e3d56e1e0bf
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-06 20:51:09 +0500

    Updated

--- DML_Commands/distinct_command.sql
+++ DML_Commands/distinct_command.sql
@@ -4,9 +4,9 @@ SELECT DISTINCT(model) AS 'All models'FROM mysqllab.smartphones;
 SELECT DISTINCT(processor_brand) AS 'ALll processors' FROM mysqllab.smartphones;
 
 -- DISTINCT combo
+-- To find unique combinations
 SELECT DISTINCT brand_name, processor_brand FROM mysqllab.smartphones;
-
--- To find unique combinations 
+ 
 SELECT DISTINCT brand_name AS 'All brands', 
 model AS 'All models', 
 processor_brand AS 'ALll processors' 
