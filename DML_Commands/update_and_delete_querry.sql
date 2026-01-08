c47014dd5f15a9823aa4eb5972ddc66e4b3f4c0d
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-08 08:09:29 +0500

    query updated

--- DML_Commands/update_and_delete_querry.sql
+++ DML_Commands/update_and_delete_querry.sql
@@ -1,4 +1,5 @@
 SELECT * FROM mysqllab.smartphones;
+
 SELECT * FROM mysqllab.userdetail;
 
 -- Update
@@ -11,3 +12,13 @@ WHERE processor_brand = 'mediatek';
 UPDATE mysqllab.userdetail
 SET email = 'shayan@gmail.com', password = '56987'
 WHERE name = 'shayan'; 
+
+-- DELETE
+-- Delete price greater than 200000
+DELETE FROM mysqllab.smartphones
+WHERE price > 200000;
+
+-- Delete samsung phones whose primary rear camera is greater than 100
+DELETE FROM mysqllab.smartphones
+WHERE primary_camera_rear > 100
+AND brand_name = 'samsung';
