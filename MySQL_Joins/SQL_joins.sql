71c45c434e3d70ef8e0a04e7ecaa48c04dcaa24f
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-15 13:12:43 +0500

    sql join query updated

--- MySQL_Joins/SQL_joins.sql
+++ MySQL_Joins/SQL_joins.sql
@@ -28,3 +28,8 @@ SELECT * FROM mysql_joins.membership t1
 RIGHT JOIN mysql_joins.users t2
 ON t1.user_id = t2.user_id;
 
+-- SELF JOIN
+SELECT * FROM mysql_joins.users1 t1
+JOIN mysql_joins.users1 t2
+ON t1.emergency_contact = t2.user_id;
+
