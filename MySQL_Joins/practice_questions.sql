e1beac386cb32b72d123abeb149890d134c415de
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-15 23:59:42 +0500

    query updated

--- MySQL_Joins/practice_questions.sql
+++ MySQL_Joins/practice_questions.sql
@@ -17,5 +17,27 @@ GROUP BY t2.name
 ORDER BY num_orders DESC LIMIT 1;
 
 -- 3. Which is the most profitable category
+SELECT t2.vertical, SUM(profit) AS 'profitable'
+FROM mysql_joins.order_details t1
+JOIN mysql_joins.category t2
+ON t1.category_id = t2.category_id
+GROUP BY t2.vertical
+ORDER BY profitable DESC LIMIt 1;
+
 -- 4. Which is the most profitable state
--- 5. Find all categories with profit higher than 5000    
+SELECT t3.state, SUM(profit) AS 'profitable_state'
+FROM mysql_joins.orders t1
+JOIN mysql_joins.order_details t2
+ON t1.order_id = t2.order_id
+JOIN mysql_joins.users t3
+ON t1.user_id = t3.user_id
+GROUP BY t3.state
+ORDER BY profitable_state DESC LIMIt 1;
+
+-- 5. Find all categories with profit higher than 5000
+SELECT t2.vertical, SUM(profit) AS 'profitable'
+FROM mysql_joins.order_details t1
+JOIN mysql_joins.category t2
+ON t1.category_id = t2.category_id
+GROUP BY t2.vertical
+HAVING profitable > 3000;  
