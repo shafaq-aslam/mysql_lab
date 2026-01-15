05f1cd633a0c1c7807bfbf134ab0542989857da1
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-15 23:30:19 +0500

    query updated

--- MySQL_Joins/join_multiple_cols_and_tables.sql
+++ MySQL_Joins/join_multiple_cols_and_tables.sql
@@ -26,11 +26,22 @@ JOIN mysql_joins.users t3
 ON t2.user_id = t3.user_id;
 
 -- Filtering Columns
+
+SELECT t1.order_id, t1.amount, t1.profit, t3.name FROM mysql_joins.order_details t1
+JOIN mysql_joins.orders t2
+ON t1.order_id = t2.order_id
+JOIN mysql_joins.users t3
+ON t2.user_id = t3.user_id;
+
 -- 1. Find order_id, name, and city by joining users and orders.
-
-
+SELECT t1.order_id, t2.name, t2.city 
+FROM mysql_joins.orders t1
+JOIN mysql_joins.users t2
+ON t1.user_id = t2.user_id;
 
 
 -- 2. Find order_id, product category by joining order_details and category.    
- 
- 
+ SELECT t1.order_id, t2.vertical 
+ FROM mysql_joins.order_details t1
+ JOIN mysql_joins.category t2
+ ON t1.category_id = t2.category_id;
