f9beb8f56a8d769d00be6a7fe7e4b9b8fa299174
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-15 12:25:55 +0500

    SQL query modified

--- MySQL_Joins/SQL_joins.sql
+++ MySQL_Joins/SQL_joins.sql
@@ -1,2 +1,20 @@
 CREATE DATABASE IF NOT EXISTS mysql_joins;
 
+-- CROSS JOIN 
+SELECT * FROM mysql_joins.users g1
+CROSS JOIN mysql_joins.groups g2;
+
+-- INNER JOIN
+SELECT * FROM mysql_joins.membership t1
+INNER JOIN mysql_joins.users t2
+ON t1.user_id = t2.user_id; 
+
+-- LEFT JOIN 
+SELECT * FROM mysql_joins.membership t1
+LEFT JOIN mysql_joins.users t2
+ON t1.user_id = t2.user_id;
+
+-- RIGHT JOIN
+SELECT * FROM mysql_joins.membership t1
+RIGHT JOIN mysql_joins.users t2
+ON t1.user_id = t2.user_id;
