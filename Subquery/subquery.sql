b6c996c964c092546752b4ddf77ce09cdb224e10
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-16 12:32:49 +0500

    query updated

--- Subquery/subquery.sql
+++ Subquery/subquery.sql
@@ -1,3 +1,12 @@
 -- Find the movie with highest rating
 SELECT * FROM mysql_subquery.movies
 WHERE score = (SELECT MAX(score) FROM mysql_subquery.movies); 
+
+-- Find the movie with highest voting
+SELECT * FROM mysql_subquery.movies
+WHERE votes = (SELECT MAX(votes) FROM mysql_subquery.movies);
+
+-- Find the movie with highest voting, and rating
+SELECT * FROM mysql_subquery.movies
+WHERE score = (SELECT MAX(score) FROM mysql_subquery.movies)
+AND votes = (SELECT MAX(votes) FROM mysql_subquery.movies);
