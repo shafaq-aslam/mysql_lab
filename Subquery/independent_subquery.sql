04b5487e51eaf4ecf2f15186ebcb50af911638cd
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-16 12:38:32 +0500

    subquery updated

---
+++ Subquery/independent_subquery.sql
@@ -0,0 +1,5 @@
+-- 1. Find the movie with highest profit (vs order by)
+-- 2. Find how many movies have a rating > the avg of all the movies ratings (Find the count of above avg movies)
+-- 3. Find the highest rated movie of 2000
+-- 4. Find the highest rated movie among all movies whose number of votes are > the dataset avg votes
+    
