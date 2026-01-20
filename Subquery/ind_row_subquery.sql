301332d9ff87207c4112c68fc8fd73353618381f
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-20 07:52:57 +0500

    independent row subquery modified

--- Subquery/ind_row_subquery.sql
+++ Subquery/ind_row_subquery.sql
@@ -1,6 +1,16 @@
 -- Independent Row Subquery (one col but multi rows)
 
 -- Find all users who never ordered
+SELECT * FROM mysql_subquery.users
+WHERE user_id NOT IN (SELECT DISTINCT(user_id) 
+					  FROM mysql_subquery.orders);
+
 -- Find all the movies made by top 3 directors (in terms of total gross incme)
+WITH top_directors AS (SELECT director FROM mysql_subquery.movies
+					GROUP BY director
+					ORDER BY SUM(gross) DESC LIMIT 3)
+SELECT * FROM mysql_subquery.movies
+WHERE director IN (SELECT * FROM top_directors);
+
 -- Find all movies of all those actors whose filmography's avg rating > 8.5 (take 25000 votes as cutoff)
   
