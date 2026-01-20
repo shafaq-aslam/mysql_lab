f88cdc82bf74c9ac54e4d1364880c88b3d47ae63
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-20 08:40:02 +0500

    new file name correlated subquery created

---
+++ Subquery/correlated_subquery.sql
@@ -0,0 +1,4 @@
+-- Correlated Subquery
+
+-- Find all the movies that have a rating higher than the average rating of movies in the same genre. [Animation[
+-- Find the favorite food of each customer.
