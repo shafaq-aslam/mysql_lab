9cfd89a0160dacff902d9fdb73f008f8d2ada2bc
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-20 07:38:41 +0500

    new query named independent row subquery added

---
+++ Subquery/ind_row_subquery.sql
@@ -0,0 +1,6 @@
+-- Independent Row Subquery
+
+-- Find all users who never ordered
+-- Find all the movies made by top 3 directors (in terms of total gross incme)
+-- Find all movies of all those actors whose filmography's avg rating > 8.5 (take 25000 votes as cutoff)
+  
