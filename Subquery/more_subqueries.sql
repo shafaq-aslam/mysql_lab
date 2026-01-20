b270d9db18400bb5ef6f34f063607b5c960f9488
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-20 23:36:10 +0500

    subquery with from updated

--- Subquery/more_subqueries.sql
+++ Subquery/more_subqueries.sql
@@ -9,3 +9,14 @@ FROM mysql_subquery.movies;
 SELECT name, genre, score, 
 (SELECT AVG(score) FROM movies m2 WHERE m2.genre = m1.genre) 
 FROM mysql_subquery.movies m1;
+
+-- WITH FROM
+
+-- Display average rating of all the restaurants
+ SELECT r_name, avg_rating
+ FROM (SELECT r_id, AVG(restaurant_rating) AS 'avg_rating'
+		 FROM mysql_subquery.orders
+		 GROUP BY r_id) t1 JOIN restaurants t2
+		 ON t1.r_id = t2.r_id;
+ 
+ 
