c22d53a0d8e8b421c17090e0f220dfb2cd473cc8
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-21 08:51:08 +0500

    new file created

---
+++ window_functions/agg_with_over().sql
@@ -0,0 +1,25 @@
+CREATE TABLE mysqllab.marks (
+	student_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    branch VARCHAR(255),
+    marks INTEGER
+);
+
+INSERT INTO mysqllab.marks (name,branch,marks)VALUES 
+('Nitish','EEE',82),
+('Rishabh','EEE',91),
+('Anukant','EEE',69),
+('Rupesh','EEE',55),
+('Shubham','CSE',78),
+('Ved','CSE',43),
+('Deepak','CSE',98),
+('Arpan','CSE',95),
+('Vinay','ECE',95),
+('Ankit','ECE',88),
+('Anand','ECE',81),
+('Rohit','ECE',95),
+('Prashant','MECH',75),
+('Amit','MECH',69),
+('Sunny','MECH',39),
+('Gautam','MECH',51)
+
