ea680d86029dde5a5805e1152cfaf0bc5a221755
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-21 22:22:08 +0500

    rank, dense rank and row number query updated

--- window_functions/rank_dense_r_row_num.sql
+++ window_functions/rank_dense_r_row_num.sql
@@ -1,10 +1,35 @@
--- RANK (Give ranking in partition)
 USE mysqllab;
--- Find top 2 most paying customers of each month
+
+-- RANK (Give ranking in partition)
 SELECT *,
 RANK() OVER(PARTITION BY branch ORDER BY marks DESC)
 FROM marks; 
 
+-- DENSE_RANK (It doesn't skip anything) 
+SELECT *,
+DENSE_RANK() OVER(PARTITION BY branch ORDER BY marks DESC)
+FROM marks;
+
+-- ROW_NUMBER 
+SELECT *,
+ROW_NUMBER() OVER(PARTITION BY branch)
+FROM marks;
+
+SELECT *,
+CONCAT(branch, '_', ROW_NUMBER() OVER(PARTITION BY branch))
+FROM marks;
+
+USE mysql_subquery;
+
+-- Find top 2 most paying customers of each month
+SELECT * FROM (SELECT MONTHNAME(date), user_id, SUM(amount) AS 'total',
+				RANK() OVER(PARTITION BY MONTHNAME(date) ORDER BY SUM(amount) DESC)
+				AS 'month_rank'
+				FROM orders
+				GROUP BY MONTHNAME(date), user_id
+				ORDER BY MONTHNAME(date)) t
+                WHERE t.month_rank < 3
+                ORDER BY user_id ASC, month_rank ASC
 -- Create roll no from branch and marks
 
 
