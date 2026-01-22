18d50902c9fc658d4d9b2e1fb4e27cca60d1e5b2
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-22 08:44:15 +0500

    query modified

--- window_functions/first_last_nth_value.sql
+++ window_functions/first_last_nth_value.sql
@@ -36,3 +36,50 @@ SELECT *,
 NTH_VALUE(name, 5) OVER(PARTITION BY branch ORDER BY marks DESC
 						ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
 FROM marks;
+
+-- Find the branch toppers 
+SELECT *,
+FIRST_VALUE(name) OVER(PARTITION BY branch ORDER BY marks DESC
+						ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+FROM marks;
+
+SELECT *,
+FIRST_VALUE(name) OVER(PARTITION BY branch ORDER BY marks DESC) 
+AS 'topper_name',
+FIRST_VALUE(marks) OVER(PARTITION BY branch ORDER BY marks DESC)
+AS 'topper_marks'
+FROM marks;
+
+SELECT * FROM (SELECT *,
+FIRST_VALUE(name) OVER(PARTITION BY branch ORDER BY marks DESC) 
+AS 'topper_name',
+FIRST_VALUE(marks) OVER(PARTITION BY branch ORDER BY marks DESC)
+AS 'topper_marks'
+FROM marks) t
+WHERE t.name = t.topper_name AND t.marks = t.topper_marks;
+
+SELECT name, branch, marks FROM (SELECT *,
+FIRST_VALUE(name) OVER(PARTITION BY branch ORDER BY marks DESC) 
+AS 'topper_name',
+FIRST_VALUE(marks) OVER(PARTITION BY branch ORDER BY marks DESC)
+AS 'topper_marks'
+FROM marks) t
+WHERE t.name = t.topper_name AND t.marks = t.topper_marks;
+
+SELECT name, branch, marks FROM (SELECT *,
+LAST_VALUE(name) OVER(PARTITION BY branch ORDER BY marks DESC
+						ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) 
+						AS 'least_name',
+LAST_VALUE(marks) OVER(PARTITION BY branch ORDER BY marks DESC
+						ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+						AS 'least_marks'
+FROM marks) t
+WHERE t.name = t.least_name AND t.marks = t.least_marks;
+
+SELECT name, branch, marks FROM (SELECT *,
+LAST_VALUE(name) OVER w AS 'least_name',
+LAST_VALUE(marks) OVER w AS 'least_marks'
+FROM marks) t
+WHERE t.name = t.least_name AND t.marks = t.least_marks
+WINDOW w AS (PARTITION BY branch ORDER BY marks DESC
+			 ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
