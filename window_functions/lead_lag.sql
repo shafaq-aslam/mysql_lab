29e16dd264d4e6ae2da3f7b0975dec45e124d9d6
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-22 08:59:34 +0500

    query modified

--- window_functions/lead_lag.sql
+++ window_functions/lead_lag.sql
@@ -1,1 +1,28 @@
 -- LEAD AND LAG 
+
+USE mysqllab;
+
+-- LAG() 
+SELECT *,
+LAG(marks) OVER(ORDER BY student_id)
+FROM marks;
+
+-- LEAD() 
+SELECT *,
+LEAD(marks) OVER(ORDER BY student_id)
+FROM marks;
+
+SELECT *,
+LEAD(marks) OVER(PARTITION BY branch ORDER BY student_id),
+LAG(marks) OVER(PARTITION BY branch ORDER BY student_id)
+FROM marks;
+
+-- Find the MoM revenue growth of Zomato
+USE mysql_subquery;
+ 
+SELECT MONTHNAME(date) AS 'month_name', 
+SUM(amount) AS 'total_amount',
+((SUM(amount) - LAG(SUM(amount)) OVER(ORDER BY MONTH(date)))/ LAG(SUM(amount)) OVER(ORDER BY MONTH(date)))*100
+FROM orders
+GROUP BY MONTH(date), MONTHNAME(date)
+ORDER BY MONTH(date) ASC;
