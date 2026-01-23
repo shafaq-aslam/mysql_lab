131a40cb68b0bf4a5059c3da29bbeafdee05c18c
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-23 08:49:19 +0500

    query modified

--- window_functions/cumulative_query.sql
+++ window_functions/cumulative_query.sql
@@ -10,3 +10,14 @@ FROM IPL
 WHERE batter = 'V Kohli'
 GROUP BY ID) t
 WHERE match_no = 'Match-10' OR match_no = 'Match-20' OR match_no = 'Match-30' ;
+
+-- Cumulative Average
+SELECT * FROM (SELECT 
+CONCAT("Match-",CAST(ROW_NUMBER() OVER(ORDER BY ID) AS CHAR)) AS 'match_no',
+SUM(batsman_run) AS 'runs_scored',
+SUM(SUM(batsman_run)) OVER w AS 'career_runs',
+AVG(SUM(batsman_run)) OVER w AS 'avg_career_runs'
+FROM IPL
+WHERE batter = 'V Kohli'
+GROUP BY ID
+WINDOW w AS (ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)) t
