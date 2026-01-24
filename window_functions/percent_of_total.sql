a351f7802792270b240c1bc488417af56bb1a46a
Shafaq Aslam <shafaqchohan123@gmail.com>
2026-01-24 08:07:46 +0500

    query updated

--- window_functions/percent_of_total.sql
+++ window_functions/percent_of_total.sql
@@ -11,4 +11,16 @@ SELECT f_name, (total_value/SUM(total_value) OVER())*100 AS 'percent_of_total'
 					GROUP BY f_id) t
 JOIN food t3
 ON t.f_id = t3.f_id
+ORDER BY (total_value/SUM(total_value) OVER())*100 DESC;
+
+
+SELECT f_name, (total_value/SUM(total_value) OVER())*100 AS 'percent_of_total'
+			FROM (SELECT f_id, SUM(amount) AS 'total_value' 
+					FROM orders t1
+					JOIN order_details t2
+					ON t1.order_id = t2.order_id
+					WHERE r_id = 4
+					GROUP BY f_id) t
+JOIN food t3
+ON t.f_id = t3.f_id
 ORDER BY (total_value/SUM(total_value) OVER())*100 DESC
